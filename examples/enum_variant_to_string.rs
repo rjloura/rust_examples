fa91aabf2b408e38c4a3bcb6dcbcd93838c51987
Rui Loura <rui@joyent.com>
2019-10-08 11:39:53 -0400

    added EnumString(from_str) to enum_variant_to_string example

--- examples/enum_variant_to_string.rs
+++ examples/enum_variant_to_string.rs
@@ -2,9 +2,11 @@ extern crate strum;
 #[macro_use]
 extern crate strum_macros;
 
+use strum::ParseError::VariantNotFound;
 use std::string::ToString;
+use std::str::FromStr;
 
-#[derive(Display, Debug)]
+#[derive(Display, EnumString, Debug, PartialEq)]
 #[strum(serialize_all = "snake_case")]
 enum Greeting {
 	HelloThere,
@@ -26,6 +28,15 @@ fn variants_to_strings() {
     assert_eq!(String::from("hello_there"), hello.to_string());
     assert_eq!(String::from("whats_up_guy"), sup_guy.to_string());
     assert_eq!(String::from("yo"), yo.to_string());
+
+    let sup_guy_variant = Greeting::from_str(&sup_guy.to_string());
+    dbg!(&sup_guy_variant);
+    assert_eq!(sup_guy_variant.unwrap(), Greeting::WhatsUpGuy);
+
+
+    let not_found_variant = Greeting::from_str(&"How_ya_doin");
+    assert_eq!(not_found_variant.unwrap_err(), VariantNotFound);
+
 }
 
 fn main() {
