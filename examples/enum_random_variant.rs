b34a0f8a804bc17e3bd162d0ba0b4fed66acb574
Rui Loura <rui@joyent.com>
2019-10-16 10:57:32 -0400

    added enum random variant

---
+++ examples/enum_random_variant.rs
@@ -0,0 +1,27 @@
+extern crate strum;
+#[macro_use]
+extern crate strum_macros;
+
+use strum::IntoEnumIterator;
+
+use rand::Rng;
+
+#[derive(Display, EnumIter, Debug)]
+enum Greeting {
+	HelloThere,
+	WhatsUpGuy,
+	Yo,
+}
+
+// Good for testing
+fn random_variant() {
+    let mut rng = rand::thread_rng();
+    let n: usize = rng.gen::<usize>() % Greeting::iter().count();
+    let random = Greeting::iter().nth(n);
+    dbg!(random);
+
+}
+
+fn main() {
+    random_variant();
+}
