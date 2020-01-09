d8dd3934f8ae695d33affc8d5c6b3abefb21a1c5
Rui Loura <rui@joyent.com>
2020-01-09 15:53:47 -0500

    added basic formatting example

---
+++ examples/formatting.rs
@@ -0,0 +1,10 @@
+/// Right Justified:
+/// |     foo     bar|
+///
+/// Left Justified:
+/// |foo     bar     |
+fn main() {
+    println!("Right Justified:\n|{:>width$}{:>width$}|", "foo", "bar", width=8);
+    println!("");
+    println!("Left Justified:\n|{:<width$}{:<width$}|", "foo", "bar", width=8);
+}
